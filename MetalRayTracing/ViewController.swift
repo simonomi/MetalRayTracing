66af46b2d96a7117f403524d7a03e9cb8a8f0010
simonomi <thesimonpellerin@gmail.com>
2023-04-02 13:48:37 -0400

    Initial Commit

---
+++ MetalRayTracing/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetalRayTracing
+//
+//  Created by simon pellerin on 2023-04-02.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
